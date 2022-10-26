aaae2717c1f1fe741a36607f9558e8596013bf03
Andrew Reynolds <areynolds00@mylangara.ca>
2022-10-26 08:57:31 -0700

    initial commit

---
+++ lab10.js
@@ -0,0 +1,49 @@
+
+///////////////////////////////
+/////// selectors /////////////
+///////////////////////////////
+
+
+let depositInupt = document.querySelector('#dField');
+
+
+
+
+//////////////////////////////
+//// Global Variables ////////
+
+
+
+//////////////////////////////
+///////// Functions //////////
+//////////////////////////////
+
+
+
+const makeDeposit = () => {
+    let depositAmount = +depositInupt.value;
+    console.log(depositAmount);
+};
+
+
+const deposit = () => {
+
+};
+
+
+
+const display = () => {
+
+};
+
+const makeWithdrawal = () => {
+
+};
+
+
+const withdraw = () => {
+
+};
+
+
+
