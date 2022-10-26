b22e75c2fa2aa6ed4e9f4c018d4165bd7c829d5f
Andrew Reynolds <andrew_reynolds@mac.com>
2022-10-26 13:52:23 -0700

    added functions

--- lab10.js
+++ lab10.js
@@ -1,49 +1,61 @@
-
 ///////////////////////////////
 /////// selectors /////////////
 ///////////////////////////////
-
-
-let depositInupt = document.querySelector('#dField');
-
-
-
-
+let balanceDisplay = document.querySelector(`#display`);
+let depositInput = document.querySelector("#dField");
+let withdrawalInput = document.querySelector(`#wField`);
+let outputMessage = document.querySelector("#message");
+let makeDepButton = document.querySelector('[value="Make Deposit"]');
+let makeWithdrawalButton = document.querySelector(`[value="Make Withdrawal"]`);
 //////////////////////////////
 //// Global Variables ////////
 
-
+let balance = 0;
 
 //////////////////////////////
 ///////// Functions //////////
 //////////////////////////////
 
-
-
 const makeDeposit = () => {
-    let depositAmount = +depositInupt.value;
-    console.log(depositAmount);
+  let depositAmount = +depositInput.value;
+  deposit(depositAmount);
+  depositInput.value = "";
 };
 
-
-const deposit = () => {
-
+const deposit = (amnt) => {
+  if (amnt <= 0 || isNaN(amnt)) {
+    display("Invalid Amount", balance);
+  } else {
+    balance += amnt;
+    display(`Your balance is: ${balance}.`, balance);
+  }
 };
 
-
-
-const display = () => {
-
+const display = (msg, acctBal) => {
+  outputMessage.innerHTML = `${msg}`;
+  balanceDisplay.innerHTML = `${balance}`;
 };
 
 const makeWithdrawal = () => {
-
+  let withdrawalAmount = +withdrawalInput.value;
+  withdraw(withdrawalAmount);
+  withdrawalInput.value = "";
 };
 
-
-const withdraw = () => {
-
+const withdraw = (amnt) => {
+  if (amnt <= 0 || isNaN(amnt)) {
+    display(`Invalid Amount`, balance);
+  } else if (amnt > balance) {
+    display(`Insufficient Funds`, balance);
+  } else {
+    balance -= amnt;
+    display(`Your balance is: ${balance}`, balance);
+  }
 };
 
+//////////////////////////////
+///////// Events /////////////
+//////////////////////////////
 
-
+makeDepButton.addEventListener("click", () => makeDeposit());
+makeWithdrawalButton.addEventListener("click", () => makeWithdrawal());
